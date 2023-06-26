ca2f3deb86e23e02f14c4c8bde2e8b410f03a63f
wahidddhrubo <wahiddhrubo@gmail.com>
2023-06-26 20:42:05 +0600

    Vercel Debug

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -12,7 +12,7 @@ const sendToken = (user, res, statusCode) => {
     //   process.env.NODE_ENV === "development"
     //     ? ".localhost"
     //     : `.${process.env.FRONTEND_DOMAIN}`,
-    sameSite: true,
+    sameSite: "none",
   };
   console.log({ statusCode, token, options });
 
