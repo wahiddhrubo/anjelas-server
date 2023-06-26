058ee203543e0b7de0293fc3423841e9b48ef76b
wahidddhrubo <wahiddhrubo@gmail.com>
2023-06-26 20:30:44 +0600

    Vercel Debug

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,9 +4,7 @@ const sendToken = (user, res, statusCode) => {
   const token = user.getJwtToken();
 
   const options = {
-    expires: new Date(
-      new Date().getTime() + process.env.COOKIE_EXPIRE * 24 * 60 * 60
-    ),
+    maxAge: process.env.PASSWORD_EXPIRE * 24 * 60 * 60 * 100,
     httpOnly: true,
     // domain:
     //   process.env.NODE_ENV === "development"
